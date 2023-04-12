f11f50dd0cd5cb315793eae57075938e6b009d91
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-04-12 18:24:39 +0700

    vinh set up

--- back_end/src/main/java/com/example/back_end/BackEndApplication.java
+++ back_end/src/main/java/com/example/back_end/BackEndApplication.java
@@ -1,13 +1,13 @@
-package com.example.web_ban_hang;
+package com.example.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebBanHangApplication {
+public class BackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebBanHangApplication.class, args);
+		SpringApplication.run(BackEndApplication.class, args);
 	}
 
 }
--- back_end/src/test/java/com/example/back_end/BackEndApplicationTests.java
+++ back_end/src/test/java/com/example/back_end/BackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.web_ban_hang;
+package com.example.back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebBanHangApplicationTests {
+class BackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
